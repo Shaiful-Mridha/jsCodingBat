7c39b8f1ca27577e57947ad27340981cb0bbda0b
Shaiful Mridha <shaifuldu555@gmail.com>
2023-11-03 13:07:06 +0200

    sum added

---
+++ array01/sum2.js
@@ -0,0 +1,21 @@
+// sum2
+// Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+
+// Examples
+
+// sum2([1, 2, 3]) → 3
+// sum2([1, 1]) → 2
+// sum2([1, 1, 1, 1]) → 2
+
+function sum2(nums) {
+  var sum = 0;
+  if (nums.length >= 2) {
+    return (sum = nums[0] + nums[1]);
+  }
+  if (nums.length < 2) {
+    return (sum = nums[0] + nums[1]);
+  }
+  if (nums.length === 0) {
+    return 0;
+  }
+}
