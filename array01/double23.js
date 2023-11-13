3729807e143159050a8a214be15f461c62685803
Shaiful Mridha <shaifuldu555@gmail.com>
2023-11-13 10:32:43 +0200

    added double23

---
+++ array01/double23.js
@@ -0,0 +1,17 @@
+// double23
+// Given an int array, return true if the array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2.
+
+// Examples
+
+// double23([2, 2]) → true
+// double23([3, 3]) → true
+// double23([2, 3]) → false
+
+function double23(nums) {
+  if (nums.length === 2) {
+    if ((nums[0] === 2 && nums[1] === 2) || (nums[0] === 3 && nums[1] === 3)) {
+      return true;
+    }
+  }
+  return false;
+}
