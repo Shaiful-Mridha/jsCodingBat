a8b5b55f4f0ef15af339421ddbc5bd05ffad5023
Shaiful Mridha <shaifuldu555@gmail.com>
2023-11-15 11:51:13 +0200

    added start1

---
+++ array01/start1.js
@@ -0,0 +1,19 @@
+// start1
+// Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
+
+// Examples
+
+// start1([1, 2, 3], [1, 3]) → 2
+// start1([7, 2, 3], [1]) → 1
+// start1([1, 2], []) → 1
+
+function start1(a, b) {
+  var sum = 0;
+  if (a[0] === 1) {
+    sum = sum + 1;
+  }
+  if (b[0] === 1) {
+    sum = sum + 1;
+  }
+  return sum;
+}
