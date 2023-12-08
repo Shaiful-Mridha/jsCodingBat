4fd5781626ee561ec7ebc5134e098d3daf8deb6d
Shaiful Mridha <shaifuldu555@gmail.com>
2023-12-08 23:05:29 +0200

    prepand Added

---
+++ array02/prependSum.js
@@ -0,0 +1,16 @@
+// prependSum
+// Return a modified version of the input array (nums), where the first two items have been removed and one item – the sum of those two items - is added to the start of the array.
+
+// Examples
+
+// prependSum([1, 2, 4, 4]) → 3,4,4
+// prependSum([3, 3, 0]) → 6,0
+// prependSum([1, 1, 1, 1, 1]) → 2,1,1,1
+
+function prependSum(nums) {
+  if (nums.length >= 2) {
+    const sum = nums[0] + nums[1];
+    nums.splice(0, 2, sum);
+  }
+  return nums;
+}
